c12d593b03097314dd567c8b0faf4c1c7a273b46
Arihant Parsoya <parsoyaarihant@gmail.com>
2020-05-27 08:32:35 +0530

    Updated copyrights

--- ProcessingQt/processing.py
+++ ProcessingQt/processing.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2019 Arihant Parsoya
+# Copyright (C) 2020 Arihant Parsoya
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
